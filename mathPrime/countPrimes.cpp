4453046595985ec6e9183138a71ec2764363cbd5
Abhishek73555 <abhishri73555@gmail.com>
2026-06-01 21:13:11 +0530

    Add countPrimes solution

---
+++ mathPrime/countPrimes.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+using namespace std;
+void fillSieve(vector<bool>& sieve){
+    int n=sieve.size()-1;
+    for(int i=2;i<=sqrt(n);i++){
+        for(int j=i*2;j<=n;j+=i)
+            sieve[j]=false;
+    }
+}
+int main(){
+    int n=100;
+    int count=0;
+    vector<bool>seive(n+1,true);
+    fillSieve(seive);
+    seive[0]=false;
+    seive[1]=false;
+    for(int i=2;i<=n;i++){
+        if(seive[i]){
+            count++;
+        }
+    }
+    cout<<count;
+}
